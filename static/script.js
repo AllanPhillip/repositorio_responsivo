1e41a26b8598d5996d6d4ecd558c15e0ca2d3111
Allan Phillip <allanphillip.ds@gmail.com>
2022-08-26 14:20:45 -0300

    Inclusão das habilidades e menu responsivo

--- static/script.js
+++ static/script.js
@@ -4,7 +4,20 @@ const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
 menuMobile.addEventListener('click', () => {
-    menuMobile.classList.contains("bi-list") ? menuMobile.classList.replace("bi-list", "bi-x") : menuMobile.classList.replace("bi-x", "bi-list"); 
-    body.classList.toggle("menu-nav-active"); 
+    menuMobile.classList.contains("bi-list") ? menuMobile.classList.replace("bi-list", "bi-x") : menuMobile.classList.replace("bi-x", "bi-list")
+    body.classList.toggle("menu-nav-active")
 
+})
+
+// Fecha o menu quando clicar em elgum item e muda o ícone para list
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list"); 
+        }
+    })
 })

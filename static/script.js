30672d0927903ce2c17b17aa68aa9f139c12afd7
Allan Phillip <allanphillip.ds@gmail.com>
2022-09-06 19:06:01 -0300

    Atualização do projeto portfolio

--- static/script.js
+++ static/script.js
@@ -45,3 +45,14 @@ window.addEventListener("scroll", ()=>{
     animeScroll(); 
 
 })
+
+// Ativar carregamento no botão de enviar formulário para 
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", ()=>{
+    btnEnviarLoader.style.display = "block"; 
+    btnEnviar.style.display = "none"; 
+
+})
